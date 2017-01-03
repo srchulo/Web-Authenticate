7c87d106c46a673b0ba60077d37af9c7c0dd124f
Adam Hopkins <violapiratejunky@gmail.com>
2017-01-02 23:06:36 -0500

    added digest class

---
+++ lib/Web/Authenticate/Digest.pm
@@ -0,0 +1,67 @@
+use strict;
+package Web::Authenticate::Digest;
+use Mouse;
+use Mouse::Util::TypeConstraints;
+use Crypt::PBKDF2;
+#ABSTRACT: The default implementation of Web::Authentication::Digest::Role.
+
+with 'Web::Authenticate::Digest::Role';
+
+=method crypt
+
+Sets the L<Crypt::PBKDF2> object that is used to create and validate digests. Below is the default:
+
+   	Crypt::PBKDF2->new(
+		hash_class => 'HMACSHA2',
+		hash_args => {
+			sha_size => 512,
+		},
+		iterations => 10000,
+		salt_len => 10,
+    )
+    
+=cut
+
+has crypt => (
+    isa => 'Crypt::PBKDF2',
+    is => 'ro',
+    required => 1,
+    default => sub {
+		Crypt::PBKDF2->new(
+			hash_class => 'HMACSHA2',
+			hash_args => {
+				sha_size => 512,
+			},
+			iterations => 10000,
+			salt_len => 10,
+		);
+    },
+);
+
+=method generate
+
+Accepts a password and returns the hex digest of that password using L</crypt>.
+
+    my $password_digest = $digest->generate($password);
+
+=cut
+
+sub generate {
+    my ($self, $password) = @_;
+	return $self->crypt->generate($password);
+}
+
+=method
+
+Uses L<Crypt::PBKDF2/"validate">.
+
+	my $validate_success = $digest->validate($hash, $password);
+
+=cut
+
+sub validate {
+	my ($self, $hash, $password) = @_;
+	return $self->crypt->validate($hash, $password);
+}
+
+1;
---
+++ lib/Web/Authenticate/Digest/Role.pm
@@ -0,0 +1,30 @@
+use strict;
+package Web::Authenticate::Digest::Role;
+use Mouse::Role;
+#ABSTRACT: A Mouse::Role that defines what methods a Web::Authenticate::Digest object should contain.
+
+=method generate
+
+All L</Web::Authenticate::Digest> objects should be able to digest text and return a hashed value.
+
+    my $hash = $digest->generate($password);
+
+=cut
+
+requires 'generate';
+
+=method
+
+Validates the stored hash for the user against the user entered password.
+
+    if ($digest->validate($hash, $password)) {
+        # success
+    } else {
+        # failure
+    }
+
+=cut
+
+requires 'validate';
+
+1;
